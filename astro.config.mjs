274d2719cf2e29cb3cc41fb6ebdb8b966a66c4d4
H4cker54n <snbq89@gmail.com>
2024-04-19 14:51:50 +0200

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://h4cker54n.github.io/cv.github.io/"
+  site: "https://cv.github.io"
 });
